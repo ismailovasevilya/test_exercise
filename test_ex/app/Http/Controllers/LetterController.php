68e724c6cbdcdb4b1b6d9ace44d8566111533b4a
ismailovasevilya <sevilya.ismailova1999@gmail.com>
2022-08-30 09:48:13 +0300

    check for 24 hours

--- test_ex/app/Http/Controllers/LetterController.php
+++ test_ex/app/Http/Controllers/LetterController.php
@@ -4,13 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Letter;
+use App\Models\User;
 use Illuminate\Support\Facades\Auth;
+use DateTime;
 
 class LetterController extends Controller
 {
     public function getIndex() {
         $letters = Letter::all();
-
         if (auth()->check() && auth()->user()->isAdmin()) {
             return redirect()->route('managerListLetters', [
                 'letters'=>$letters
@@ -26,16 +27,24 @@ class LetterController extends Controller
     }
 
     public function postCreate(Request $req) {
+        $user_id = Auth::id();
+       
+        if ($this->checkTime($user_id)) {
+            $letter = new Letter([
+                'message' => $req->input('message'),
+                'topic' => $req->input('topic'),
+                'user_id' => Auth::id($user_id)
+            ]);
+            $letter->save();
+            return redirect()->route('ReadOwnLetter', [
+                'id'=>$letter->id
+            ]);
+        }
+        else {
+            echo '24 hours hasnt passed';
+        }
         
-        $letter = new Letter([
-            'message' => $req->input('message'),
-            'topic' => $req->input('topic'),
-            'user_id' => Auth::id()
-        ]);
-        $letter->save();
-        return redirect()->route('ReadOwnLetter', [
-            'id'=>$letter->id
-        ]);
+        
     }
 
     public function managerListLetters(Request $req) {
@@ -51,5 +60,25 @@ class LetterController extends Controller
             'letter'=>$letter
         ]);
     }
+
+    public function checkTime($user_id) {
+        if(Letter::where('user_id', $user_id)->exists()) {
+            $lastLetter = Letter::orderBy('created_at', 'desc')->where( 'user_id', $user_id)->first();
+            $lastLetterTime = $lastLetter->created_at;
+
+            $currentTime = new DateTime("now");
+            $diff = $currentTime->diff($lastLetterTime);
+
+            $hours = $diff->h;
+            if ($hours >= 2) {
+                return TRUE;
+            }
+            else {
+                return FALSE;
+            }
+        } else {
+            return TRUE;
+        }  
+    }        
     
 }
